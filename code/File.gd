8e1bffd2f16a70fb50efe45113e505f8d99b6b08
viz <142146272+nexacopic@users.noreply.github.com>
2024-09-22 17:50:43 +0100

    pretty much done

--- code/File.gd
+++ code/File.gd
@@ -1,10 +1,65 @@
 extends Control
+@export var full_path = ""
+@export var type = "txt"
+
 
 func _unhandled_input(event: InputEvent) -> void:
+	if(!visible):
+		return;
+	type = full_path.get_extension()
 	if event is InputEventKey and event.pressed:
 		var e = event as InputEventKey
 		if event.is_pressed():
-			if e.keycode == KEY_S and Input.is_key_pressed(KEY_CTRL):
-				# TODO: Save
-				pass
-			
+			if e.keycode == KEY_S and Input.is_action_pressed("modifier"):
+				save()
+			if e.keycode == KEY_S and Input.is_action_pressed("modifier") and Input.is_action_pressed("shiftmod"):
+				saveas()
+			if e.keycode == KEY_X and Input.is_action_pressed("modifier"):
+				save(false)
+				queue_free()
+
+func visible_to_real_filename(filename: String) -> String:
+	var regex = RegEx.new()
+	regex.compile(r"(.+)\s\((\w+)\)")
+	
+	var match = regex.search(filename)
+	
+	if match:
+		var base_name = match.get_string(0)
+		var extension = match.get_string(1)
+		return base_name + "." + extension
+	else:
+		return filename
+		
+func real_to_visible_filename(filename: String) -> String:
+	var splits = filename.split(".")
+	if splits.size() == 2:
+		return splits[0] + " (" + splits[1] + ")"
+	else:
+		return filename
+
+func save(savease = true):
+	var filename = full_path
+	if(filename=="newfiledontsavecorrectly"):
+		if(savease): saveas()
+		return;
+	
+	DirAccess.remove_absolute(filename)
+	var f = FileAccess.open(filename, FileAccess.WRITE)
+	f.store_string($CodeEdit.text)
+	name = filename.get_basename().get_file() + " (" + filename.get_extension() + ")"
+	
+
+func saveas():
+	var getFile = FileDialog.new()
+	getFile.title = "save as"
+	getFile.file_mode = FileDialog.FILE_MODE_SAVE_FILE
+	getFile.access = FileDialog.ACCESS_FILESYSTEM
+	getFile.use_native_dialog = true
+	getFile.file_selected.connect(func(path):full_path = path; save())
+	getFile.popup()
+
+
+func _on_code_edit_text_changed() -> void:
+	if("*" not in name):
+		name += " *"
--- code/tab_container_keybinds.gd
+++ code/tab_container_keybinds.gd
@@ -1,9 +1,40 @@
 extends TabContainer
+var selectedfile = ""
+
+
 func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventKey and event.pressed:
 		var e = event as InputEventKey
 		if event.is_pressed():
-			if e.keycode == KEY_LEFT and Input.is_key_pressed(KEY_CTRL):
+			if e.keycode == KEY_LEFT and Input.is_action_pressed("modifier"):
 				current_tab = max(current_tab - 1, 0)
-			elif e.keycode == KEY_RIGHT and Input.is_key_pressed(KEY_CTRL):
+			elif e.keycode == KEY_RIGHT and Input.is_action_pressed("modifier"):
 				current_tab = min(current_tab + 1, get_tab_count() - 1)
+			elif e.keycode == KEY_N and Input.is_action_pressed("modifier"):
+				var f = get_parent().get_node("filepreview").duplicate()
+				add_child(f)
+				f.name = "newfile (txt)"
+				f.full_path = "newfiledontsavecorrectly"
+				f.visible = true
+			elif e.keycode == KEY_O and Input.is_action_pressed("modifier"):
+				var getFile = FileDialog.new()
+				getFile.title = "open file"
+				getFile.file_mode = FileDialog.FILE_MODE_OPEN_FILE
+				getFile.access = FileDialog.ACCESS_FILESYSTEM
+				getFile.use_native_dialog = true
+				getFile.file_selected.connect(func(path):
+					var f = get_parent().get_node("filepreview").duplicate()
+					add_child(f)
+					f.name = path.get_basename().get_file() + " (" + path.get_extension() + ")"
+					f.full_path = path
+					var file = FileAccess.open(path, FileAccess.READ)
+					f.get_node("CodeEdit").text = file.get_as_text()
+					f.visible = true
+				)
+				getFile.popup()
+
+
+func _on_tab_changed(tab: int) -> void:
+	selectedfile = get_child(tab).name
+	print("file changed " + selectedfile)
+	DisplayServer.window_set_title("grade - " + selectedfile)
